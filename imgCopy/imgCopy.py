93371f5a86687578a7ca8430be34591067961758
Mailblocker <Mailblocker@web.de>
2020-02-29 14:05:56 +0100

    Added threading for non blocking log update while copying files

--- imgCopy/imgCopy.py
+++ imgCopy/imgCopy.py
@@ -9,10 +9,52 @@ import argparse
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout
 from PyQt5.QtCore import QSize
-from PyQt5.Qt import QPushButton, QFileDialog, QFileInfo, QTextEdit
+from PyQt5.Qt import QPushButton, QFileDialog, QFileInfo, QTextEdit, QThread,\
+    pyqtSignal
 from shutil import copyfile
 import os
 import glob
+
+class copyThread(QThread):
+    newText = pyqtSignal(str)
+    
+    def __init__(self, source = '', destination = ''):
+        QThread.__init__(self)
+        self.sourcePath = source
+        self.destinationPath = destination
+        
+    def run(self):
+        file = QFileInfo(self.sourcePath)
+            
+        lines = [file.path() + '/' + line.rstrip('\n') for line in open(file.absoluteFilePath())]
+        self.newText.emit('--------------------------------------------------------------------------------')
+        self.newText.emit('Now copying {} files.'.format(len(lines)))
+    
+        count = 1
+        for x in lines:
+            if QFileInfo(x).exists():
+                y = self.destinationPath + '/IMG_' + "{:04}".format(count) + '.' + QFileInfo(x).suffix()
+                self.newText.emit('copying {} to {}'.format(x, y))
+                copyfile(x, y)
+                count = count + 1
+            else:
+                mList = glob.glob(file.path() + '/**/' + os.path.basename(x), recursive=True)
+                if 1 == len(mList):
+                    # Found 1 file we will use this one
+                    x = mList[0]
+                    y = self.destinationPath + '/IMG_' + "{:04}".format(count) + '.' + QFileInfo(x).suffix()
+                    self.newText.emit('copying {} to {}'.format(x, y))
+                    copyfile(x, y)
+                    count = count + 1
+                elif 0 == len(mList):
+                    self.newText.emit('File {} could not be found, will be skipped.'.format(x))
+                else:
+                    self.newText.emit('Multiple files for {} have been found, file will be skipped.'.format(x))
+        
+        if(len(lines) != count-1):
+            self.newText.emit('Warning: Could not copy all files!')
+        self.newText.emit('Finished copying {} files of {}'.format((count-1), len(lines)))
+        self.newText.emit('--------------------------------------------------------------------------------')
 
 class MainWindow(QMainWindow):
     def __init__(self, src, dest):
@@ -49,14 +91,17 @@ class MainWindow(QMainWindow):
             
         if None != dest:
             self.selectDestination(dest)
-        
+            
+        self.copyThread = copyThread(src, dest)
+        self.copyThread.newText.connect(self.writeText)
+                
     def selectSource(self, src=None):
         if src == None:
             self.sourcePath, _ = QFileDialog.getOpenFileName(None, 'Choose the file to work with', '', 'Image list .txt (*.txt);; * (*.*)', '')
         else:
             self.sourcePath = src
         self.source.setText("Source: " + self.sourcePath)
-        self.textOutput.insertPlainText("Source selected: " + self.sourcePath + "\n")
+        self.textOutput.append("Source selected: " + self.sourcePath)
     
     def selectDestination(self, dest=None):
         if dest == None:
@@ -64,46 +109,20 @@ class MainWindow(QMainWindow):
         else:
             self.destinationPath = dest
         self.destination.setText("Destination: " + self.destinationPath)
-        self.textOutput.insertPlainText("Destination: " + self.destinationPath + "\n")
-        
+        self.textOutput.append("Destination: " + self.destinationPath)
+
     def copyFiles(self):
         if self.sourcePath is not None and self.destinationPath is not None:
-            file = QFileInfo(self.sourcePath)
-    
-            lines = [file.path() + '/' + line.rstrip('\n') for line in open(file.absoluteFilePath())]
-            self.textOutput.insertPlainText('--------------------------------------------------------------------------------\n')
-            self.textOutput.insertPlainText('Now copying ' + str(len(lines)) + ' files.' + '\n')
-    
-            count = 1
-            for x in lines:
-                if QFileInfo(x).exists():
-                    y = self.destinationPath + '/IMG_' + "{:04}".format(count) + '.' + QFileInfo(x).suffix()
-                    self.textOutput.insertPlainText('copying ' + x + ' to ' + y + "\n")
-                    copyfile(x, y)
-                    count = count + 1
-                else:
-                    mList = glob.glob(file.path() + '/**/' + os.path.basename(x), recursive=True)
-                    if 1 == len(mList):
-                        # Found 1 file we will use this one
-                        x = mList[0]
-                        y = self.destinationPath + '/IMG_' + "{:04}".format(count) + '.' + QFileInfo(x).suffix()
-                        self.textOutput.insertPlainText('copying ' + x + ' to ' + y + "\n")
-                        copyfile(x, y)
-                        count = count + 1
-                    elif 0 == len(mList):
-                        self.textOutput.insertPlainText('File ' + x + ' could not be found, will be skipped.' + "\n")
-                    else:
-                        self.textOutput.insertPlainText('Multiple files for ' + x + ' have been found, will be skipped.' + "\n")
-            
-            if(len(lines) != count-1):
-                self.textOutput.insertPlainText('Warning: Could not copy all files!\n')
-            self.textOutput.insertPlainText('Finished copying ' + str(count-1) + ' files of ' + str(len(lines)) + "\n")
-            self.textOutput.insertPlainText('--------------------------------------------------------------------------------\n')
-            
+            self.copyThread.sourcePath = self.sourcePath
+            self.copyThread.destinationPath = self.destinationPath
+            self.copyThread.start()
         else:
-            self.textOutput.insertPlainText('Please select a source file and a destination folder.' + "\n")
+            self.textOutput.append('Please select a source file and a destination folder.')
+          
+    def writeText(self, line):
+        self.textOutput.append(line)
+        self.textOutput.ensureCursorVisible()
             
-
 def main(argv):
     app = QtWidgets.QApplication(argv)
     parser = argparse.ArgumentParser(description='Copy images defined by a .txt file to a destination.')
@@ -118,4 +137,3 @@ def main(argv):
 
 if __name__ == '__main__':
     main(sys.argv)
-    
